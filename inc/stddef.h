51e3f2526e712dc125da1ae5ceb38b8db0630ade
Patrick Pokatilo <shyxormz@asxsoft.de>
2010-10-22 08:11:18 +0200

    stddef.h
    + added stddef.h (and NULL)
    
    Signed-off-by: Patrick Pokatilo <shyxormz@asxsoft.de>

---
+++ inc/stddef.h
@@ -0,0 +1,30 @@
+/*
+ *  ASXSoft Nuke - Operating System - kernel5 - Codename: 理コ込
+ *  Copyright (C) 2010 Patrick Pokatilo
+ *
+ *  This file is part of Nuke (理コ込).
+ *
+ *  Nuke (理コ込) is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nuke (理コ込) is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nuke (理コ込).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _STDDEF_H_
+#define _STDDEF_H_
+
+#ifndef __cplusplus
+#define NULL (void *)0
+#else
+#define NULL 0
+#endif
+
+#endif
