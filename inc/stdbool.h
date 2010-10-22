f5a50dbd7fc2c3ab316ebe88487da5b06f65920a
Patrick Pokatilo <shyxormz@asxsoft.de>
2010-10-22 08:07:35 +0200

    bool added
    + bool added to stdbool.h
    
    Signed-off-by: Patrick Pokatilo <shyxormz@asxsoft.de>

---
+++ inc/stdbool.h
@@ -0,0 +1,32 @@
+/*
+ *  ASXSoft Nuke - Operating System - kernel5 - Codename: 理コ込
+ *  Copyright (C) 2010 Patrick Pokatilo
+ *
+ *  This file is part of Nuke (理コ込).
+ *
+ *  Nuke (理コ込) is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nuke (理コ込) is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nuke (理コ込).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _STDBOOL_H_
+#define _STDBOOL_H_
+
+#define __bool_true_false_are_defined 1
+
+#ifndef __cplusplus
+#define bool _Bool
+#define true 1
+#define false 0
+#endif
+
+#endif
